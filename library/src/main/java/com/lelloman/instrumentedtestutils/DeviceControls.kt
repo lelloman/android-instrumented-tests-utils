aefaf08b5f85115741b0cf345e358776353eb400
lelloman <cerasuolodomenico@gmail.com>
2018-12-30 14:53:19 +0100

    init

---
+++ library/src/main/java/com/lelloman/instrumentedtestutils/DeviceControls.kt
@@ -0,0 +1,10 @@
+package com.lelloman.instrumentedtestutils
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.uiautomator.UiDevice
+
+fun rotateNatural() = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).setOrientationNatural()
+
+fun rotateLeft() = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).setOrientationLeft()
+
+fun rotateRight() = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).setOrientationRight()
---
+++ library/src/main/java/com/lelloman/instrumentedtestutils/ViewActions.kt
@@ -0,0 +1,19 @@
+package com.lelloman.instrumentedtestutils
+
+import android.support.test.espresso.ViewAction
+import android.support.test.espresso.action.CoordinatesProvider
+import android.support.test.espresso.action.GeneralSwipeAction
+import android.support.test.espresso.action.Press
+import android.support.test.espresso.action.Swipe
+
+
+fun swipeBottomSheetUp(): ViewAction = GeneralSwipeAction(
+    Swipe.FAST,
+    CoordinatesProvider {
+        floatArrayOf(200f, 1750f)
+    },
+    CoordinatesProvider {
+        floatArrayOf(400f, 700f)
+    },
+    Press.FINGER
+)
