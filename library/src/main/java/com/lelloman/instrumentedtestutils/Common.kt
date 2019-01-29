2b9aaab098ff3248cf0566b8942a88dcb1defdd8
lelloman <cerasuolodomenico@gmail.com>
2019-01-29 08:28:13 +0100

    Replaced dexmaker with dexopener and updated gradle + gradle plugin

--- library/src/main/java/com/lelloman/instrumentedtestutils/Common.kt
+++ library/src/main/java/com/lelloman/instrumentedtestutils/Common.kt
@@ -5,3 +5,10 @@ import org.mockito.stubbing.OngoingStubbing
 
 
 fun <T> whenever(methodCall: T): OngoingStubbing<T> = Mockito.`when`(methodCall)
+
+@Suppress("UNCHECKED_CAST")
+fun <T> nullAsT(): T = null as T
+
+inline fun <reified T : Any> nonNullAny(): T {
+    return Mockito.any(T::class.java) ?: nullAsT()
+}
